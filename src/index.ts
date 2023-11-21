1f62ab596b01ce4b714d303dd86696bb0e9852e0
Murillo Nahás <murillonahas2003@gmail.com>
2023-11-21 11:43:46 -0300

    feat: using routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import Koa from "koa";
+import { router } from "./router";
 
 const app = new Koa();
 
 const port = 3000;
 
+app.use(router.routes());
+
 app.listen(port, () => {
-  console.log(`🚀 Server is running on port ${port}`);
+  console.log(`🚀 Server is running on port http://localhost:${port}/`);
 });
