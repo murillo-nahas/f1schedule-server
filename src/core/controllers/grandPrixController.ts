5e746ac64425ea9adcebcc7e4fd8ae000c9fb7cc
Murillo Nahás <murillonahas2003@gmail.com>
2023-12-01 22:23:18 -0300

    feat: improving controller

--- src/core/controllers/grandPrixController.ts
+++ src/core/controllers/grandPrixController.ts
@@ -5,30 +5,38 @@ import { GrandPrix } from '../models/grandPrixModel'
 export class GrandPrixController {
   constructor(private readonly grandPrixRepository: GrandPrixRepository) {}
 
-  async getAll(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+  async getAll(_: FastifyRequest, reply: FastifyReply): Promise<void> {
     const grandPrixList = await this.grandPrixRepository.getAll()
-    reply.send(grandPrixList)
+    reply.status(200).send(grandPrixList)
   }
 
   async getById(request: FastifyRequest, reply: FastifyReply): Promise<void> {
-    const { id } = request.params as { id: string }
+    try {
+      const { id } = request.params as { id: string }
 
-    const grandPrix = await this.grandPrixRepository.getById(parseInt(id))
+      const grandPrix = await this.grandPrixRepository.getById(parseInt(id))
 
-    if (!grandPrix) {
-      reply.status(404).send({ message: 'ID not found!' })
-      return
+      if (!grandPrix) {
+        reply.status(404).send({ message: 'ID not found!' })
+        return
+      }
+
+      reply.status(200).send(grandPrix)
+    } catch (error) {
+      reply.status(404).send({ message: 'Grand Prix not found.' })
     }
-
-    reply.status(200).send(grandPrix)
   }
 
   async create(request: FastifyRequest, reply: FastifyReply): Promise<void> {
-    const newGrandPrix = await this.grandPrixRepository.create(
-      request.body as GrandPrix,
-    )
+    try {
+      const newGrandPrix = await this.grandPrixRepository.create(
+        request.body as GrandPrix,
+      )
 
-    reply.status(201).send(newGrandPrix)
+      reply.status(201).send(newGrandPrix)
+    } catch (error) {
+      reply.status(500).send({ message: 'Internal Server Error.' })
+    }
   }
 
   async update(request: FastifyRequest, reply: FastifyReply) {
@@ -41,27 +49,21 @@ export class GrandPrixController {
         data,
       )
 
-      if (!updatedGrandPrix) {
-        reply.status(404).send({ message: 'ID not found!' })
-        return
-      }
-
       reply.status(200).send(updatedGrandPrix)
     } catch (error) {
-      reply.status(500).send({ message: 'Internal Server Error' })
+      reply.status(404).send({ message: 'Grand Prix not found.' })
     }
   }
 
   async remove(request: FastifyRequest, reply: FastifyReply): Promise<void> {
-    const { id } = request.params as { id: string }
+    try {
+      const { id } = request.params as { id: string }
 
-    const grandPrix = await this.grandPrixRepository.remove(parseInt(id))
+      await this.grandPrixRepository.remove(parseInt(id))
 
-    if (!grandPrix) {
-      reply.status(404).send({ message: 'ID not found!' })
-      return
+      reply.status(204)
+    } catch (error) {
+      reply.status(404).send({ message: 'Grand Prix not found.' })
     }
-
-    reply.status(200).send(grandPrix)
   }
 }
