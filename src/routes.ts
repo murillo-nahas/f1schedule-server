3e2903fba5cac6d20010050be064b168daf44059
Murillo Nahás <murillonahas2003@gmail.com>
2023-12-04 09:48:59 -0300

    feat: adding practice routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { GrandPrixController } from '../src/core/controllers/grandPrixController'
+import { PracticeController } from './core/controllers/practiceController'
 
-export const setupRoutes = (
+export const setupGrandPrixRoutes = (
   fastify: FastifyInstance,
   controller: GrandPrixController,
 ) => {
@@ -10,4 +11,15 @@ export const setupRoutes = (
   fastify.post('/grandprix', controller.create.bind(controller))
   fastify.put('/grandprix/:id', controller.update.bind(controller))
   fastify.delete('/grandprix/:id', controller.remove.bind(controller))
+}
+
+export const setupPracticeRoutes = (
+  fastify: FastifyInstance,
+  controller: PracticeController,
+) => {
+  fastify.get('/practice', controller.getAll.bind(controller))
+  fastify.get('/practice/:id', controller.getById.bind(controller))
+  fastify.post('/practice', controller.create.bind(controller))
+  fastify.put('/practice/:id', controller.update.bind(controller))
+  fastify.delete('/practice/:id', controller.remove.bind(controller))
 }
